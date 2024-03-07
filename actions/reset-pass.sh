463dbe66f02e06d6229969a8b6322962d9239d07
Shadowy Super Coder <musashidisciple@proton.me>
2024-03-06 18:23:25 -0700

    reset-pass with freshly generated pass

--- actions/reset-pass.sh
+++ actions/reset-pass.sh
@@ -3,9 +3,10 @@
 set -e
 
 SUPERUSER_ACCOUNT_ID=$(sqlite3 ./data/database.sqlite3 'select super_user from settings;')
-DEFAULT_PASS=$(cat /app/data/start9/admin_password.txt)
+ADMIN_PASS=$(cat /dev/urandom | base64 | head -c 24)
+echo "$ADMIN_PASS" > /app/data/start9/admin_password.txt
 CURRENT_DATETIME=$(date +%s)
-PASS_HASH=$(python3 -c "import bcrypt; print(bcrypt.hashpw('$DEFAULT_PASS'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))")
+PASS_HASH=$(python3 -c "import bcrypt; print(bcrypt.hashpw('$ADMIN_PASS'.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'))")
 
 sqlite3 ./data/database.sqlite3 <<EOF
 UPDATE accounts
@@ -17,7 +18,7 @@ EOF
 
 action_result_complete="    {
     \"version\": \"0\",
-    \"message\": \"Superuser Password has been reset to the default password.  You can find this password within Properties.\",
+    \"message\": \"Superuser Password has been reset to a newly generated random password available in Properties.\",
     \"value\": null,
     \"copyable\": false,
     \"qr\": false
