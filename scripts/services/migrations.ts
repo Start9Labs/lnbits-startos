a45a8c017b58ca64bd94dde4dacc497be8e679ad
Shadowy Super Coder <musashidisciple@proton.me>
2024-10-30 11:32:09 -0600

    Update to 0.12.12

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -1,23 +1,26 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations
-.fromMapping({
-    "0.9.7.2": {
+export const migration: T.ExpectedExports.migration =
+  compat.migrations.fromMapping(
+    {
+      "0.9.7.2": {
         up: compat.migrations.updateConfig(
-            (config: any) => {
-                return { implementation: config?.wallet.type || 'LndRestWallet' }
-            },
-            true,
-            { version: "0.9.7.2", type: "up" },
+          (config: any) => {
+            return { implementation: config?.wallet.type || "LndRestWallet" };
+          },
+          true,
+          { version: "0.9.7.2", type: "up" }
         ),
         down: compat.migrations.updateConfig(
-            (config: any) => {
-                return { wallet: { type: config.implementation || 'LndRestWallet' } };
-            },
-            true,
-            { version: "0.9.7.2", type: "down" },
+          (config: any) => {
+            return {
+              wallet: { type: config.implementation || "LndRestWallet" },
+            };
+          },
+          true,
+          { version: "0.9.7.2", type: "down" }
         ),
-  },
-},
-"0.12.11",
-);
+      },
+    },
+    "0.12.12"
+  );
