4a825af2f664cefe6d4a1c242363f2ee1727bff5
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-03 14:25:23 -0600

    add username to resetPassword ActionResult

--- startos/actions/resetPassword.ts
+++ startos/actions/resetPassword.ts
@@ -20,8 +20,7 @@ export const resetPassword = sdk.Action.withoutInput(
   // the execution function
   async ({ effects }) => {
     const newPassword = utils.getDefaultString(randomPassword)
-
-    await sdk.SubContainer.withTemp(
+    const res = await sdk.SubContainer.withTemp(
       effects,
       { imageId: 'lnbits' },
       mainMounts,
@@ -41,18 +40,23 @@ export const resetPassword = sdk.Action.withoutInput(
 
         const currentTime = await subc.execFail(['date', '+%s'])
 
-        const res = await subc.execFail([
+        await subc.execFail([
           'sqlite3',
           db,
           `update accounts set password_hash = \"${newPasswordHash.stdout.toString().trimEnd()}\", updated_at = ${currentTime.stdout.toString().trimEnd()} where id = ${superuserAccountId.stdout.toString().trimEnd()};`,
         ])
 
+        return await subc.execFail([
+          'sqlite3',
+          db,
+          `select username from accounts where id = ${superuserAccountId.stdout.toString().trimEnd()};`,
+        ])
       },
     )
     return {
       version: '1',
       title: 'Success',
-      message: 'The Super User new password is below',
+      message: `The new Super User password for '${res.stdout}' is below`,
       result: {
         type: 'single',
         value: newPassword,
