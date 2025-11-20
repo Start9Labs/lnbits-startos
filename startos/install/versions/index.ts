6828d011e04b043585f09d532954750d50de84bd
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-19 22:24:20 -0700

    bump pre-release

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,4 @@
 import { v1_2_1_1 } from './v1_2_1_1'
-export { v1_3_1_1_alpha1 as current } from './v1_3_1_1-alpha.1'
+export { v1_3_1_1_beta0 as current } from './v1_3_1_1-beta.0'
 
 export const other = [v1_2_1_1]
--- startos/install/versions/v1_3_1_1-beta.0.ts
+++ startos/install/versions/v1_3_1_1-beta.0.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v1_3_1_1_alpha1 = VersionInfo.of({
-  version: '1.3.1:1-alpha.1',
+export const v1_3_1_1_beta0 = VersionInfo.of({
+  version: '1.3.1:1-beta.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     down: IMPOSSIBLE,
