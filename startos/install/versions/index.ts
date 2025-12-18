051c216b08f10ddf01a8a624f9c92c6e668160bb
Shadowy Super Coder <musashidisciple@proton.me>
2025-12-17 17:55:18 -0700

    version cleanup

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,4 +1,3 @@
-import { v1_2_1_1 } from './v1_2_1_1'
-export { v1_3_1_1_beta2 as current } from './v1_3_1_1-beta.2'
+export { v1_3_1_1 as current } from './v1.3.1_1-beta.0'
 
-export const other = [v1_2_1_1]
+export const other = []
--- startos/install/versions/v1.3.1_1-beta.0.ts
+++ startos/install/versions/v1.3.1_1-beta.0.ts
@@ -2,10 +2,10 @@ import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 import { envFile } from '../../fileModels/env'
 import { envDefaults } from '../../utils'
 import { load } from 'js-yaml'
-import { readFile } from 'fs/promises'
+import { readFile, rm } from 'fs/promises'
 
-export const v1_2_1_1 = VersionInfo.of({
-  version: '1.2.1:1-alpha.0',
+export const v1_3_1_1 = VersionInfo.of({
+  version: '1.2.1:1-beta.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {
@@ -29,6 +29,10 @@ export const v1_2_1_1 = VersionInfo.of({
           LNBITS_BACKEND_WALLET_CLASS: configuredImplementation,
           LNBITS_ALLOWED_FUNDING_SOURCES: configuredImplementation,
         })
+
+        rm('/media/startos/volumes/main/start9', {
+          recursive: true,
+        }).catch(console.error)
       } catch {
         console.log('config.yaml not found')
       }
