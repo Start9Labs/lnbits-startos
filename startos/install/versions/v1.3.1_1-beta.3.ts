c11eecaf231ef19b2ae85ff9a61d4fef5566a446
Matt Hill <mattnine@protonmail.com>
2025-12-21 18:33:35 -0700

    better migratio

--- startos/install/versions/v1.3.1_1-beta.3.ts
+++ startos/install/versions/v1.3.1_1-beta.3.ts
@@ -1,7 +1,6 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
 import { envFile } from '../../fileModels/env'
 import { envDefaults } from '../../utils'
-import { load } from 'js-yaml'
 import { readFile, rm } from 'fs/promises'
 
 export const v1_3_1_1 = VersionInfo.of({
@@ -9,16 +8,17 @@ export const v1_3_1_1 = VersionInfo.of({
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {
-      const raw = await readFile(
+      // get old config.yaml
+      const configYaml:
+        | {
+            implementation: 'LndRestWallet' | 'CLightningWallet'
+          }
+        | undefined = await readFile(
         '/media/startos/volumes/main/start9/config.yaml',
-        'utf8',
-      ).catch(console.log)
+        'utf-8',
+      ).then(YAML.parse, () => undefined)
 
-      if (raw) {
-        const configYaml = load(raw) as {
-          implementation: 'LndRestWallet' | 'CLightningWallet'
-        }
-
+      if (configYaml) {
         const configuredImplementation =
           configYaml.implementation === 'CLightningWallet'
             ? 'CoreLightningWallet'
@@ -29,11 +29,11 @@ export const v1_3_1_1 = VersionInfo.of({
           LNBITS_BACKEND_WALLET_CLASS: configuredImplementation,
           LNBITS_ALLOWED_FUNDING_SOURCES: configuredImplementation,
         })
-      }
 
-      rm('/media/startos/volumes/main/start9', {
-        recursive: true,
-      }).catch(console.error)
+        rm('/media/startos/volumes/main/start9', {
+          recursive: true,
+        }).catch(console.error)
+      }
     },
     down: IMPOSSIBLE,
   },
